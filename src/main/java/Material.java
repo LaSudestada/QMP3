28e7a836123a2efe48f601ea5bb615fc2780fe9b
Ulises Benjamin Suhr <ulisessuhr@hotmail.com>
2024-05-16 00:29:13 -0300

    Agrego materiales y tipos para poder usar en pruebas

--- src/main/java/Material.java
+++ src/main/java/Material.java
@@ -2,9 +2,9 @@ import java.util.Objects;
 
 public class Material {
 //  JEAN,
-//  ALGODON, LINO,
+//  ALGODON, LINO, PIQUE
 //  GABARDINA, GASA, LONA,
-//  POLIESTER, NEOPRENO,
+//  POLIESTER, NEOPRENO, ACETATO
 //  SATEN, SEDA, TERCIOPELO,
 //  LANA, ALPACA
   Trama trama;
@@ -17,4 +17,14 @@ public class Material {
     this.trama = Objects.requireNonNullElse(trama, Trama.LISA);
   }
 
+  //WKO
+  public static Material JEAN = new Material(Trama.LISA);
+
+  public static Material ALGODON_LISO = new Material(Trama.LISA);
+  public static Material ALGODON_RAYADO = new Material(Trama.RAYADA);
+  public static Material ALGODON_ESTAMPADO = new Material(Trama.ESTAMPADA);
+
+  public static Material PIQUE_LISO = new Material(Trama.LISA);
+  public static Material PIQUE_RAYADO = new Material(Trama.RAYADA);
+  public static Material PIQUE_CUADRILLE = new Material(Trama.CUADRILLE);
 }
--- src/main/java/Tipo.java
+++ src/main/java/Tipo.java
@@ -1,8 +1,8 @@
 public class Tipo {
 //  ZAPATO, ZAPATILLA, BOTA, SANDALIA, OJOTA,
 //  CAMISA_MANGA_LARGA, CAMISA_MANGA_CORTA, REMERA, CHOMBA,
-//  SACO, CAMPERA, BUZO, SWEATER, CHQUETA,
-//  PANTALON_VAQUERO, PANTALON_JOGGIN, CALZA, VERMUDA, SHORT
+//  SACO, CAMPERA, BUZO, SWEATER,
+//  PANTALON, CALZA, VERMUDA, SHORT, POLLERA, FALDA
 
   Categoria categoria;
 
@@ -13,4 +13,27 @@ public class Tipo {
   public Tipo(Categoria categoria) {
     this.categoria = categoria;
   }
+
+
+  public static Tipo ZAPATO = new Tipo(Categoria.CALZADO);
+  public static Tipo ZAPATILLA = new Tipo(Categoria.CALZADO);
+  public static Tipo BOTA = new Tipo(Categoria.CALZADO);
+  public static Tipo SANDALIA = new Tipo(Categoria.CALZADO);
+  public static Tipo OJOTA = new Tipo(Categoria.CALZADO);
+
+  public static Tipo PANTALON = new Tipo(Categoria.PARTE_INFERIOR);
+  public static Tipo CALZA = new Tipo(Categoria.PARTE_INFERIOR);
+  public static Tipo VERMUDA = new Tipo(Categoria.PARTE_INFERIOR);
+  public static Tipo SHORT = new Tipo(Categoria.PARTE_INFERIOR);
+  public static Tipo POLLERA = new Tipo(Categoria.PARTE_INFERIOR);
+  public static Tipo FALDA = new Tipo(Categoria.PARTE_INFERIOR);
+
+  public static Tipo CAMISA_MANGA_LARGA = new Tipo(Categoria.PARTE_SUPERIOR);
+  public static Tipo CAMISA_MANGA_CORTA = new Tipo(Categoria.PARTE_SUPERIOR);
+  public static Tipo REMERA = new Tipo(Categoria.PARTE_SUPERIOR);
+  public static Tipo CHOMBA = new Tipo(Categoria.PARTE_SUPERIOR);
+  public static Tipo SACO = new Tipo(Categoria.PARTE_SUPERIOR);
+  public static Tipo CAMPERA = new Tipo(Categoria.PARTE_SUPERIOR);
+  public static Tipo BUZO = new Tipo(Categoria.PARTE_SUPERIOR);
+  public static Tipo SWEATER = new Tipo(Categoria.PARTE_SUPERIOR);
 }
