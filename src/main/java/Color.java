81b12cb75b08b76b88a9f7bc8ce6b1156be266e3
Ulises Benjamin Suhr <ulisessuhr@hotmail.com>
2024-05-17 01:29:42 -0300

    Agrego validacion de colores

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -6,9 +6,17 @@ public class Color {
   private int blue;
 
   public Color(int red, int green, int blue) {
-    this.red = red;
-    this.green = green;
-    this.blue = blue;
+    if(esColorValido(red) && esColorValido(green) && esColorValido(blue)) {
+      this.red = red;
+      this.green = green;
+      this.blue = blue;
+    } else {
+      throw new RuntimeException("Los colores seleccionados no son validos");
+    }
+  }
+
+  private boolean esColorValido(int color) {
+    return color >= 0 && color <= 255;
   }
 
   public int[] getColor() {
